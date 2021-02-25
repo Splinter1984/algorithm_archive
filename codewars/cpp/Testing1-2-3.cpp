84bdf963a9fbc7ff5600b1c708be08dde70f8f40
Andrianov Roma <47949756+Splinter1984@users.noreply.github.com>
2021-02-25 19:53:26 +0700

    Create Testing1-2-3.cpp

---
+++ codewars/cpp/Testing1-2-3.cpp
@@ -0,0 +1,24 @@
+/*Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+
+Write a function which takes a list of strings and returns each line prepended by the correct number.
+
+The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+Examples:
+
+number(List<string>()) // => List<string>()
+number(List<string>{"a", "b", "c"}) // => ["1: a", "2: b", "3: c"]*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+std::vector<std::string> number(const std::vector<std::string> &lines)
+{  
+    std::vector<std::string> res;
+    for (int i=0; i < lines.size(); i++)
+    {
+        res.push_back(std::to_string(i + 1) + ": " + lines[i]);
+    }
+    return res;
+}
